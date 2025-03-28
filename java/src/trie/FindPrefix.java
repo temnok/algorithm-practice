a4a001546cd54ce4e5796c6bb9b196d63ad57af0
onemchyn <olena.nemchynska@avid.com>
2025-03-28 21:30:13 +0200

    Mocks for task: find if the string from array is substring of another string

---
+++ java/src/trie/FindPrefix.java
@@ -0,0 +1,7 @@
+package trie;
+
+public class FindPrefix {
+	public static boolean hasPrefix(String[] arr) {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ java/src/trie/FindPrefixTest.java
@@ -0,0 +1,10 @@
+package trie;
+
+import org.junit.Test;
+
+public class FindPrefixTest {
+	@Test
+	public void testRandomCases() {
+
+	}
+}
