835892784734320c8e8f21a3cdee601570af60a9
Alex Temnok <temnok@gmail.com>
2025-06-29 15:39:08 -0700

    Go: bfs stubs

---
+++ go/bfs/min_dist.go
@@ -0,0 +1,7 @@
+package bfs
+
+// minDist should return the minimal distance between start and end nodes in an arbitrary graph
+// represented as an adjacency list, or -1 if there is no path from start to end nodes
+func minDist(graph [][]int, start, end int) int {
+	return 0 // TODO
+}
---
+++ go/bfs/min_path.go
@@ -0,0 +1,7 @@
+package bfs
+
+// minPath should return list of nodes of the minimal path from start to end nodes in an arbitrary graph
+// represented as an adjacency list, or empty list if there is no such path
+func minPath(graph [][]int, start, end int) []int {
+	return nil // TODO
+}
