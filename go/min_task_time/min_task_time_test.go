87ad78c90afececc01960b8f9c065b0124c09e80
Alex Temnok <temnok@gmail.com>
2025-06-29 19:55:23 -0700

    Go: tests for min_task_time

---
+++ go/min_task_time/min_task_time_test.go
@@ -0,0 +1,23 @@
+package min_task_time
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestExample(t *testing.T) {
+	assert.Equal(t, 120, minTaskTime(5,
+		[]int{20, 10, 30, 50, 40},
+		[][]int{{3, 4}, {2, 3}, {1, 0}, {2, 1}, {3, 1}, {3, 0}}))
+}
+
+func TestRandomCases(t *testing.T) {
+	for range 10_000 {
+		td := newRandomTestData()
+		actual := minTaskTime(td.n, td.time, td.before)
+		if td.expected != actual {
+			assert.FailNowf(t, "", "minTaskTime(%v, %v, %v):\nwant %v\n got %v",
+				td.n, td.time, td.before, td.expected, actual)
+		}
+	}
+}
---
+++ go/min_task_time/random_test_data.go
@@ -0,0 +1,88 @@
+package min_task_time
+
+import (
+	"math/rand/v2"
+)
+
+type randomTestData struct {
+	n, expected int
+	time        []int
+	before      [][]int
+}
+
+var random = rand.New(rand.NewPCG(0, 0))
+
+func newRandomTestData() *randomTestData {
+	var td randomTestData
+
+	for !td.randomTestDataSuccess() {
+	}
+
+	return &td
+}
+
+func (td *randomTestData) randomTestDataSuccess() bool {
+	td.n = 1 + random.IntN(100)
+	td.time = make([]int, td.n)
+	for u := range td.n {
+		td.time[u] = 1 + random.IntN(100)
+	}
+
+	var edges [][]int
+	mat := make([][]int, td.n+1)
+	for i := range td.n {
+		mat[i] = make([]int, td.n+1)
+	}
+
+	for i, m := 0, random.IntN(td.n*2); i < m; i++ {
+		u, v := random.IntN(td.n), random.IntN(td.n)
+		if mat[u][v] > 0 {
+			continue
+		}
+
+		edges = append(edges, []int{u, v})
+		mat[u][v] = td.time[u]
+	}
+
+	for u := range td.n {
+		edges = append(edges, []int{u, td.n})
+		mat[u][td.n] = td.time[u]
+	}
+
+	for m := 0; m <= td.n; m++ {
+		for u := 0; u <= td.n; u++ {
+			if mat[u][m] == 0 {
+				continue
+			}
+
+			for v := 0; v <= td.n; v++ {
+				if mat[m][v] == 0 {
+					continue
+				}
+
+				var w = mat[u][m] + mat[m][v]
+				if -w < -mat[u][v] {
+					mat[u][v] = w
+				}
+			}
+		}
+	}
+
+	td.expected = 0
+	for u := 0; u <= td.n; u++ {
+		if mat[u][u] > 0 {
+			return false
+		}
+
+		for v := 0; v <= td.n; v++ {
+			if mat[u][v] > td.expected {
+				td.expected = mat[u][v]
+			}
+		}
+	}
+
+	td.before = edges[:len(edges)-td.n]
+
+	return true
+
+}
