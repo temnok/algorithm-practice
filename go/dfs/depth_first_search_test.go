5390af53abfbbd9e6b27cc474829454c7275a53a
Alex Temnok <temnok@gmail.com>
2025-06-29 19:05:38 -0700

    Go: tests for dfs

---
+++ go/dfs/depth_first_search_test.go
@@ -0,0 +1,45 @@
+package dfs
+
+import (
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"testing"
+)
+
+func TestDfs(t *testing.T) {
+	graph := [][]int{
+		{1, 3},
+		{2, 4},
+		{5},
+		{4, 6},
+		{5, 7},
+		{8},
+		{7},
+		{8},
+		{2},
+	}
+	expected1 := []int{0, 1, 2, 5, 8, 4, 7, 3, 6}
+	expected2 := []int{0, 3, 6, 7, 8, 2, 5, 4, 1}
+
+	actual := dfs(graph, 0)
+	if !reflect.DeepEqual(actual, expected1) && !reflect.DeepEqual(actual, expected2) {
+		assert.FailNowf(t, "", "dfs(%v, 0):\n got %v\nwant %v or %v", graph, actual, expected1, expected2)
+	}
+}
+
+func TestRandomCases(t *testing.T) {
+	for range 10_000 {
+		td := newRandomTestData()
+
+		expected1 := td.order1
+		expected2 := td.order2
+		actual := dfs(td.graph, td.start)
+
+		if !reflect.DeepEqual(expected1, actual) && !reflect.DeepEqual(expected2, actual) {
+			assert.FailNowf(t, "",
+				"dfs(%v, %v):\nwant %v or %v\ngot %v",
+				td.graph, td.start, expected1, expected2, actual,
+			)
+		}
+	}
+}
---
+++ go/dfs/random_test_data.go
@@ -0,0 +1,80 @@
+package dfs
+
+import (
+	"math/rand/v2"
+)
+
+type randomTestData struct {
+	graph          [][]int
+	start          int
+	order1, order2 []int
+}
+
+var random = rand.New(rand.NewPCG(0, 0))
+
+func newRandomTestData() *randomTestData {
+	var td randomTestData
+
+	n, maxAdj := 1+random.IntN(50), random.IntN(50)
+	for range n {
+		var adj []int
+
+		m := random.IntN(1 + maxAdj)
+		for range m {
+			v := random.IntN(n)
+			adj = append(adj, v)
+		}
+
+		td.graph = append(td.graph, adj)
+	}
+
+	td.start = random.IntN(n)
+	td.walk1()
+	td.walk2()
+
+	return &td
+}
+
+func (td *randomTestData) walk1() {
+	var stack []int
+	visited := make([]bool, len(td.graph))
+
+	stack = append(stack, td.start)
+	visited[td.start] = true
+
+	for len(stack) > 0 {
+		u := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		td.order1 = append(td.order1, u)
+
+		for _, v := range td.graph[u] {
+			if !visited[v] {
+				stack = append(stack, v)
+				visited[v] = true
+			}
+		}
+	}
+}
+
+func (td *randomTestData) walk2() {
+	var stack []int
+	visited := make([]bool, len(td.graph))
+
+	stack = append(stack, td.start)
+	visited[td.start] = true
+
+	for len(stack) > 0 {
+		u := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		td.order1 = append(td.order1, u)
+
+		row := td.graph[u]
+		for i := len(row) - 1; i >= 0; i-- {
+			v := row[i]
+			if !visited[v] {
+				stack = append(stack, v)
+				visited[v] = true
+			}
+		}
+	}
+}
