60e99a665d4d070fa7d835737d97bb591d65e7cd
onemchyn <olena.nemchynska@avid.com>
2025-07-12 09:22:43 +0300

    -

--- java/src/string_search/HashingTest.java
+++ java/src/string_search/HashingTest.java
@@ -5,39 +5,6 @@ import org.junit.Test;
 
 public class HashingTest {
 	@Test
-	public void getHashesTest() {
-		String s = "dasfasf;jadfja;dfjasdfj";
-		int size = 5;
-		long[] hashes = Hashing.getHashes(s, size);
-		Assert.assertEquals(s.length() - size + 1, hashes.length);
-
-		for (int i = 0; i < s.length() - size + 1; i++) {
-			String str = s.substring(i, i + size);
-			long h = 0;
-			for (int j = 0; j < str.length(); j++) {
-				h = h*Hashing.BASE + Hashing.toInt(str.toCharArray()[j]);
-			}
-
-			Assert.assertEquals(h, hashes[i]);
-
-		}
-	}
-
-	@Test
-	public void getHashTest() {
-		String s = "iwcwiuig";
-
-		long[] hashes = Hashing.getHashes(s, s.length());
-
-		long h = 0;
-		for (int j = 0; j < s.length(); j++) {
-			h = h*Hashing.BASE + Hashing.toInt(s.toCharArray()[j]);
-		}
-
-		Assert.assertEquals(h, hashes[0]);
-	}
-
-	@Test
 	public void testSearchRandomHashing() {
 		for (int i = 0 ; i < 10000; i++) {
 			randomTestData td = new randomTestData();
