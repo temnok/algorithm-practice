c713f7eb0cd6906342127df39eae88516f1c38aa
onemchyn <olena.nemchynska@avid.com>
2025-07-15 04:10:01 +0300

    remove implementation

--- java/src/bfs/BreadthFirstSearch.java
+++ java/src/bfs/BreadthFirstSearch.java
@@ -19,23 +19,6 @@ public class BreadthFirstSearch {
 	//
 	// and start node 0, the result should be { 0, 1, 3, 2, 4, 6, 5, 7, 8 }
 	public static List<Integer> bfs(List<List<Integer>> graph, int start) {
-		int queue[] = new int[graph.size()];
-		int l = 0, r = 0;
-		boolean [] visited = new boolean[graph.size()];
-		queue[r++] = start;
-		visited[start] = true;
-		List<Integer> result = new ArrayList<>();
-
-		while (l < r) {
-			int cur = queue[l++];
-			result.addLast(cur);
-			for (int i : graph.get(cur)) {
-				if (!visited[i]) {
-					queue[r++] = i;
-					visited[i] = true;
-				}
-			}
-		}
-		return result;
+		throw new UnsupportedOperationException();
 	}
 }
