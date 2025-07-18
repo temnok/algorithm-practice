8461b4e36dda61ee51e9abfa8654d28702c474a3
onemchyn <olena.nemchynska@avid.com>
2025-07-18 23:34:35 +0300

    fixes

--- java/src/dijkstra/BellmanFordMinPathTest.java
+++ java/src/dijkstra/BellmanFordMinPathTest.java
@@ -11,7 +11,7 @@ public class BellmanFordMinPathTest {
 		for (int i = 0; i < 10_000; i++) {
 			var td = new randomTestData();
 
-			var actual = BellmanFordMinPath.bellmanFordPath(td.n, td.edges, td.start);
+			var actual = BellmanFordMinPath.bellmanFordMinPath(td.n, td.edges, td.start);
 			if (!Arrays.equals(actual, td.expected)) {
 				Assert.fail(String.format(
 					"dijkstraMinPath(%s, %s, %s):\nwant %s\n got %s",
