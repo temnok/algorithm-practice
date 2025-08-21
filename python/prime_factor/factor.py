22fe23c3e7f73152ed205f4bfacad2cb9765866c
Alex Temnok <temnok@gmail.com>
2025-08-21 15:10:43 -0700

    Python: prime factors

---
+++ python/prime_factor/factor.py
@@ -0,0 +1,25 @@
+
+
+# list_smallest_prime_factors should return *the smallest prime factor*
+# for each number i < n, or 0 if number i is a prime or numbers 0 or 1.
+# For example, for n = 26, the answer should be
+# [0, 0, 0, 0, 2, 0, 2, 0, 2, 3, 2, 0, 2, 0, 2, 3, 2, 0, 2, 0, 2, 3, 2, 0, 2, 5]
+def list_smallest_prime_factors(n: int) -> list[int]:
+	# raise NotImplementedError('TODO')
+
+	factors = [0]*n
+	primes = []
+
+	for i in range(2, n):
+		f = factors[i]
+		if f == 0:
+			f = i
+			primes.append(i)
+
+		for p in primes:
+			if p > f or p*i >= n:
+				break
+
+			factors[p*i] = p
+
+	return factors
