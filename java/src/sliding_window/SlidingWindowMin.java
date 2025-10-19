a005830ab606d4315c3e9de76030026b4b3b6bee
Alex Temnok <temnok@gmail.com>
2025-10-19 02:06:34 -0700

    Revert "Sliding window minimum, Go and Java: reverting solutions"
    This reverts commit bc73619b6d74365150fec556019e569c60a083e3.

--- java/src/sliding_window/SlidingWindowMin.java
+++ java/src/sliding_window/SlidingWindowMin.java
@@ -1,6 +1,9 @@
 package sliding_window;
 
+import java.util.Stack;
+
 public class SlidingWindowMin {
+
 	// slidingWindowMin should return minimum for each position of
 	// the sliding window of length n on array arr. For example,
 	// if arr = [-1, 2, 1, 0, 5, -10] and n = 3,
@@ -11,6 +14,66 @@ public class SlidingWindowMin {
 	//   - len(arr) >= n
 	//   - arr can contain any integer
 	public static int[] slidingWindowMin(int[] arr, int n) {
-		throw new UnsupportedOperationException("TODO");
+		var q = new MinQueue();
+
+		for (var i = 0; i < n-1; i++) {
+			q.add(arr[i]);
+		}
+
+		var ans = new int[arr.length - n + 1];
+
+		for (var i = 0; i < ans.length; i++) {
+			q.add(arr[i+n-1]);
+			ans[i] = q.min();
+			q.remove();
+		}
+
+		return ans;
+	}
+}
+
+class MinStack {
+	Stack<int[]> stack = new Stack<>();
+
+	boolean isEmpty() {
+		return stack.isEmpty();
+	}
+
+	int min() {
+		if (isEmpty()) {
+			return Integer.MAX_VALUE;
+		}
+
+		return stack.lastElement()[1];
+	}
+
+	void push(int val) {
+		stack.push(new int[] {val, Math.min(val, min())});
+	}
+
+	int pop() {
+		return stack.pop()[0];
+	}
+}
+
+class MinQueue {
+	MinStack l = new MinStack(), r = new MinStack();
+
+	int min() {
+		return Math.min(l.min(), r.min());
+	}
+
+	void add(int val) {
+		r.push(val);
+	}
+
+	int remove() {
+		if (l.isEmpty()) {
+			while (!r.isEmpty()) {
+				l.push(r.pop());
+			}
+		}
+
+		return l.pop();
 	}
 }
