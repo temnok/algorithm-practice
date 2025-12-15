b6995eeabd53124ad1a62ab0d072e898cb92ced8
Alex Temnok <temnok@gmail.com>
2025-12-14 16:14:37 -0800

    Refactor expr solution

--- go/expr/expr.go
+++ go/expr/expr.go
@@ -5,71 +5,77 @@ package expr
 // binary operators '+', '-', '*', '/'. Multiply and division have higher priority than add/sub.
 // Division by zero produces zero result. For example, expression '1+2*(3/0-4)' evaluates to -7.
 func EvaluateExpr(expr string) int {
+	return rpnEval(rpnFromExpr(expr))
+}
+
+func rpnFromExpr(expr string) []byte {
+	var rpn, stack []byte
+
 	pri := map[byte]int{
-		'*': 3,
-		'/': 3,
+		'(': 0,
+		')': 1,
 		'+': 2,
 		'-': 2,
-		')': 1,
-		'(': 0,
+		'*': 3,
+		'/': 3,
 	}
 
-	var rp, stack []byte
 	for _, op := range []byte(expr) {
 		switch {
 		case op >= '0' && op <= '9':
-			rp = append(rp, op)
+			rpn = append(rpn, op)
 
 		case op == '(':
-			stack = append(stack, '(')
+			stack = append(stack, op)
 
 		default:
-			sp := len(stack)
-			for ; sp > 0 && pri[stack[sp-1]] >= pri[op]; sp-- {
-				rp = append(rp, stack[sp-1])
+			sp := len(stack) - 1
+			for ; sp >= 0 && pri[stack[sp]] >= pri[op]; sp-- {
+				rpn = append(rpn, stack[sp])
 			}
 
-			stack = stack[:sp]
-
 			if op == ')' {
-				stack = stack[:sp-1]
+				stack = stack[:sp]
 			} else {
-				stack = append(stack, op)
+				stack = append(stack[:sp+1], op)
 			}
 		}
 	}
 
 	for sp := len(stack) - 1; sp >= 0; sp-- {
-		rp = append(rp, stack[sp])
+		rpn = append(rpn, stack[sp])
 	}
 
-	var calcStack []int
+	return rpn
+}
 
-	for _, op := range rp {
+func rpnEval(rpn []byte) int {
+	var stack []int
+
+	for _, op := range rpn {
 		if op >= '0' && op <= '9' {
-			calcStack = append(calcStack, int(op-'0'))
+			stack = append(stack, int(op-'0'))
 			continue
 		}
 
-		sp := len(calcStack) - 2
-		a, b := calcStack[sp], calcStack[sp+1]
+		sp := len(stack) - 2
+		a, b, c := stack[sp], stack[sp+1], 0
+
 		switch op {
 		case '+':
-			calcStack[sp] = a + b
+			c = a + b
 		case '-':
-			calcStack[sp] = a - b
+			c = a - b
 		case '*':
-			calcStack[sp] = a * b
+			c = a * b
 		case '/':
-			if b == 0 {
-				calcStack[sp] = 0
-			} else {
-				calcStack[sp] = a / b
+			if b != 0 {
+				c = a / b
 			}
 		}
 
-		calcStack = calcStack[:sp+1]
+		stack = append(stack[:sp], c)
 	}
 
-	return calcStack[0]
+	return stack[0]
 }
