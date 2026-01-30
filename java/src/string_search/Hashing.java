52e6de6e6c05437969fd9f8a418ede46c6ce210d
onemchyn <olena.nemchynska@avid.com>
2026-01-30 02:16:59 +0200

    fixes

--- java/src/string_search/Hashing.java
+++ java/src/string_search/Hashing.java
@@ -2,10 +2,34 @@ package string_search;
 
 public class Hashing {
 	public static int search(String source, String substring) {
-		throw new UnsupportedOperationException("TODO");
+		char sourceArray[] = source.toCharArray();
+		char substringArray[] = substring.toCharArray();
+
+		int base = 31;
+		long pow_base = 1;
+		long sourceHash = 0;
+		long substringHash = 0;
+
+		for (int i = substring.length() - 1; i >= 0; i--) {
+			sourceHash = toInt(sourceArray[i]) * pow_base;
+			substringHash = toInt(substringArray[i]) * pow_base;
+			pow_base = pow_base * base;
+		}
+
+		for (int i = 0; i < sourceArray.length - substringArray.length; i++) {
+			if (sourceHash == substringHash) {
+				return i;
+			}
+
+			sourceHash = sourceHash * base;
+			sourceHash -= toInt(sourceArray[i])* pow_base;
+			sourceHash += sourceArray[i + substringArray.length];
+		}
+
+		return -1;
 	}
 
 	public static int toInt(char c) {
-		throw new UnsupportedOperationException("TODO");
+		return c + 1;
 	}
 }
