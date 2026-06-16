41ed419152f828ba9b35ae3c7cf8417ea1340da1
Alex Temnok <temnok@gmail.com>
2026-06-15 21:07:34 -0700

    Java: Levenshtein distance: revert solution

--- java/src/string/distance/Levenshtein.java
+++ java/src/string/distance/Levenshtein.java
@@ -9,32 +9,6 @@ public class Levenshtein {
     // distance("kitten", "sitting") must return 3
     //
     public static int distance(String a, String b) {
-        var m = a.length();
-        var n = b.length();
-
-        var cur = new int[n+1];
-        for (var j = 0; j <= n; j++) {
-            cur[j] = j;
-        }
-
-        var prev = new int[n+1];
-        for (var i = 0; i < m; i++) {
-            var tmp = prev; prev = cur; cur = tmp;
-
-            cur[0] = i+1;
-
-            for (var j = 0; j < n; j++) {
-                var x = prev[j];
-                if (a.charAt(i) != b.charAt(j)) {
-                    x++;
-                }
-
-                var y = Math.min(cur[j], prev[j+1])+1;
-                cur[j+1] = Math.min(x, y);
-            }
-
-        }
-
-        return cur[n];
+        throw new UnsupportedOperationException("TODO");
     }
 }
